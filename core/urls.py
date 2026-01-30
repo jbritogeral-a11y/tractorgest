391975fa0d21928f61ddbef8775b85d9e29e00aa
jbritogeral-a11y <jbrito.geral@gmail.com>
2026-01-30 20:41:01 +0000

    Adicionar pagina de login separada

--- core/urls.py
+++ core/urls.py
@@ -3,5 +3,6 @@ from django.urls import path, include  # <--- Não esqueças de importar o inclu
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls')), # Adiciona as rotas de login/logout padrão
     path('', include('producao.urls')), # <--- Isto define que a página inicial vai para a produção
 ]
